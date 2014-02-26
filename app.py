9136bec92bb964e7a1b5403b78328be2e7cb5db3
Hackbright Student <info@hackbrightacademy.com>
2014-02-25 16:31:54 -0800

    added a database and flash messages

--- app.py
+++ app.py
@@ -6,15 +6,34 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/") # Default is GET request
 def index():
-    return render_template("index.html")
+    if session.get("username"):
+        return "User %s is logged in!" % session['username']
+    else:
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    username = model.authenticate(username, password)
+
+    if username != None:
+        flash("User authenticated!") 
+        session['username'] = username
+    else:
+        flash("Password incorrect, there may be an issue.") 
+    
+    return redirect(url_for("index"))
 
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,8 +1,23 @@
+import sqlite3
+
+DB = None
+CONN = None
+
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("thewall.db")
+    DB = CONN.cursor()
+
 ADMIN_USER="hackbright"
-ADMIN_PASSWORD=803096023
+ADMIN_PASSWORD=5980025637247534551
 
 def authenticate(username, password):
-    if username == ADMIN_USER and hash(password) == ADMIN_PASSWORD:
-        return ADMIN_USER
+    connect_to_db()
+    query = """SELECT username, password FROM users WHERE username = ?"""
+    DB.execute(query, (username,))
+    row = DB.fetchone()
+
+    if username == row[0] and hash(password) == int(row[1]):
+        return username
     else:    
         return None
